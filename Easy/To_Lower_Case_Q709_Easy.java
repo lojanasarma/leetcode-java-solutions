4e494674210730a9012fcc869bc5441ad03e770a
Lojana sarma Saravanabhava sarma <lojanasarma@gmail.com>
2026-04-17 07:21:38 +0530

    Add files via upload
    To Lower Case Question 709 Easy
    Link : https://leetcode.com/problems/to-lower-case/description/

---
+++ Easy/To_Lower_Case_Q709_Easy.java
@@ -0,0 +1,16 @@
+
+package To_Lower_Case_Q709_Easy;
+
+ /* I completed this question using the concepts learned in the
+"Introduction to Programming in Java" module, which I studied
+in Year 1 Semester 1.*/
+
+class Solution {
+    public String toLowerCase(String s) {
+
+        String lower = s.toLowerCase();
+
+        return lower;
+
+    }
+}
